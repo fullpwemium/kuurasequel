71dc799697cebaf6592c65dad3cf0481e0500691
jsniemela <niemelajonne@gmail.com>
2016-11-07 19:25:53 +0200

    Added scene for cutscenes.

---
+++ Assets/Cutscenes/Scripts/EventHandler.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventHandler : MonoBehaviour {
+    public GameObject background;
+    public int currentScene = 1;
+    public int scenePosition = 0;
+    // Use this for initialization
+    void Start () {
+        
+        switch (currentScene)
+        {
+            case 1:
+                break;
+            case 2:
+                break;
+            default:
+                break;
+
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+}
