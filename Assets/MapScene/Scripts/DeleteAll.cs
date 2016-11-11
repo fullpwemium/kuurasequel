95ccc1d259e54b5249ee91f611f2a04f2516f050
ALEKSINKONE\xxx <yrttiaho.aleksi@gmail.com>
2016-11-11 13:42:54 +0200

    Created Delete All button which deletes saved magic dust and completed levels.

---
+++ Assets/MapScene/Scripts/DeleteAll.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class DeleteAll : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void ClearAll()
+    {
+        PlayerPrefs.DeleteAll();
+        //GlobalGameManager.MagicDust = 1000;
+        //PlayerPrefs.SetInt("Magic Dust ", GlobalGameManager.MagicDust);
+        SceneManager.LoadScene("Map2");
+    }
+}
