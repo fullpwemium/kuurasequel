c9da949168707b8c270a79126e76e9a0e2e09724
Peter22FIN <peterkorkko@hotmail.com>
2016-11-23 01:49:19 +0200

    Added extra dust from ads and also fixed IAP. If you face compile errors from IAPs just comment the whole cs files
    Added extra dust from ads (+100), needs daily check or something and
    also fixed IAP. If you face compile errors from IAPs comment following
    files: IAPButton.cs, IAPDemo.cs, IAPButtonEditor.cs and Purchaser.cs and
    contact me

--- Assets/Plugins/UnityPurchasing/Editor/IAPButtonEditor.cs
+++ Assets/Plugins/UnityPurchasing/Editor/IAPButtonEditor.cs
@@ -1,77 +1,85 @@
-﻿//using UnityEditor;
-//using UnityEngine;
-//using System.IO;
-//using System.Collections.Generic;
+﻿using UnityEditor;
+using UnityEngine;
+using System.IO;
+using System.Collections.Generic;
 
-//namespace UnityEngine.Purchasing
-//{
-//	public static class IAPButtonMenu
-//	{
-//		[MenuItem ("Window/Unity IAP/Create IAP Button", false, 5)]
-//		public static void CreateUnityIAPButton()
-//		{
-//			// Create Button
-//			EditorApplication.ExecuteMenuItem("GameObject/UI/Button");
+namespace UnityEngine.Purchasing
+{
+    public static class IAPButtonMenu
+    {
+        [MenuItem("Window/Unity IAP/Create IAP Button", false, 5)]
+        public static void CreateUnityIAPButton()
+        {
+            // Create Button
+            EditorApplication.ExecuteMenuItem("GameObject/UI/Button");
 
-//			// Get GameObject of Button
-//			GameObject gO = Selection.activeGameObject;
+            // Get GameObject of Button
+            GameObject gO = Selection.activeGameObject;
 
-//			// Add IAP Button component to GameObject
-//			IAPButton iapButton = null;
-//			if (gO) {
-//				iapButton = gO.AddComponent<IAPButton>();
-//			}
+            // Add IAP Button component to GameObject
+            IAPButton iapButton = null;
+            if (gO)
+            {
+                iapButton = gO.AddComponent<IAPButton>();
+            }
 
-//			if (iapButton != null) {
-//				UnityEditorInternal.ComponentUtility.MoveComponentUp(iapButton);
-//				UnityEditorInternal.ComponentUtility.MoveComponentUp(iapButton);
-//				UnityEditorInternal.ComponentUtility.MoveComponentUp(iapButton);
-//			}
-//		}
-//	}
+            if (iapButton != null)
+            {
+                UnityEditorInternal.ComponentUtility.MoveComponentUp(iapButton);
+                UnityEditorInternal.ComponentUtility.MoveComponentUp(iapButton);
+                UnityEditorInternal.ComponentUtility.MoveComponentUp(iapButton);
+            }
+        }
+    }
 
 
-//	[CustomEditor(typeof(IAPButton))]
-//	public class IAPButtonEditor : Editor 
-//	{
-//		private static readonly string[] excludedFields = new string[] { "m_Script" };
-//		private const string kNoProduct = "<None>";
-//		private List<string> validIDs = null;
+    [CustomEditor(typeof(IAPButton))]
+    public class IAPButtonEditor : Editor
+    {
+        private static readonly string[] excludedFields = new string[] { "m_Script" };
+        private const string kNoProduct = "<None>";
+        private List<string> validIDs = null;
 
-//		public override void OnInspectorGUI()
-//		{
-//			IAPButton button = (IAPButton)target;
+        public override void OnInspectorGUI()
+        {
+            IAPButton button = (IAPButton)target;
 
-//			serializedObject.Update();
+            serializedObject.Update();
 
-//			EditorGUILayout.LabelField(new GUIContent("Product ID:", "Select a product from the IAP catalog"));
+            EditorGUILayout.LabelField(new GUIContent("Product ID:", "Select a product from the IAP catalog"));
 
-//			if (validIDs == null) {
-//				var catalog = ProductCatalog.LoadDefaultCatalog();
+            if (validIDs == null)
+            {
+                var catalog = ProductCatalog.LoadDefaultCatalog();
 
-//				validIDs = new List<string>();
-//				validIDs.Add(kNoProduct);
-//				foreach (var product in catalog.allProducts) {
-//					validIDs.Add(product.id);
-//				}
-//			}
+                validIDs = new List<string>();
+                validIDs.Add(kNoProduct);
+                foreach (var product in catalog.allProducts)
+                {
+                    validIDs.Add(product.id);
+                }
+            }
 
-//			int currentIndex = string.IsNullOrEmpty(button.productId) ? 0 : validIDs.IndexOf(button.productId);
-//			int newIndex = EditorGUILayout.Popup(currentIndex, validIDs.ToArray());
-//			if (newIndex > 0 && newIndex < validIDs.Count) {
-//				button.productId = validIDs[newIndex];
-//			} else {
-//				button.productId = string.Empty;
-//			}
+            int currentIndex = string.IsNullOrEmpty(button.productId) ? 0 : validIDs.IndexOf(button.productId);
+            int newIndex = EditorGUILayout.Popup(currentIndex, validIDs.ToArray());
+            if (newIndex > 0 && newIndex < validIDs.Count)
+            {
+                button.productId = validIDs[newIndex];
+            }
+            else
+            {
+                button.productId = string.Empty;
+            }
 
-//			if (GUILayout.Button("IAP Catalog...")) {
-//				ProductCatalogEditor.ShowWindow();
-//			}
-			
-//			DrawPropertiesExcluding(serializedObject, excludedFields);
+            if (GUILayout.Button("IAP Catalog..."))
+            {
+                ProductCatalogEditor.ShowWindow();
+            }
 
-//			serializedObject.ApplyModifiedProperties();
-//		}
-//	}
-//}
+            DrawPropertiesExcluding(serializedObject, excludedFields);
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
 
