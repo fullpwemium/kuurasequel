55ab6049f30cd719f860bf9cd9d92a77897388f7
vilkaikk <ville.kaikk@gmail.com>
2017-05-02 17:22:23 +0300

    Replaced all the placeholder assets with hand-drawn ones. Fixed bugs, added code documentation and created some UI animations to add some flavor to the Word Quiz Minigame.

---
+++ KuuraInTheGlobe-master/Assets/WordGame/scripts/ButtonAnimation.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonAnimation : MonoBehaviour {
+
+    private float timeLeft;             //determines the intervals when the wiggling animation will be played
+    private Animator anim;              //reference to the animator
+
+    /**
+     * Initializes the animator and the first wiggle timer.
+     */
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        timeLeft = Random.Range(4.0f, 7.5f);
+    }
+
+    // Update is called once per frame
+    /**
+     * Counts from the timeLeft down to 0. When the timer reaches 0, the button will wiggle.
+     */
+    void Update(){
+       
+        timeLeft -= Time.deltaTime;
+
+        if (timeLeft < 0)
+        {
+            changeState();
+        }
+    }
+
+    /**
+     * Generates a new animation timer between 2 random floats and triggers the animations itself.
+     */
+    private void changeState()
+    {
+        timeLeft = Random.Range(6.5f, 20.5f);           //refresh the timer once again
+        anim.SetTrigger("animWiggle");
+    }
+}
--- KuuraInTheGlobe-master/Assets/WordGame/scripts/WordGameScript.cs
+++ KuuraInTheGlobe-master/Assets/WordGame/scripts/WordGameScript.cs
@@ -1,11 +1,12 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+/**
+ * This script is the main gameplay loop for the Word Quiz Minigame.
+ */
 public class WordGameScript : MonoBehaviour {
 
     //Variables used to initialize the game system
@@ -23,7 +24,7 @@ public class WordGameScript : MonoBehaviour {
     private int tries;
     private int currentLevel = 0;
     private List<int> questionIndexes;
-    private int lastIndex;              //Stores the last attempt's first question index here, so when if the level is restarted, the player wont be given the same question twice in a row
+    private int lastIndex;              //Stores the previous attempt's last question index here, so if the level is restarted, the player wont reveive the same question twice in a row
 
     //UI objects
     private Image buttonA;
@@ -36,6 +37,7 @@ public class WordGameScript : MonoBehaviour {
     public GameObject GameOverPanel;
     public GameObject NextLevelPanel;
     public GameObject CatPanel;
+    public GameObject FinalPanel;
 
     //Variables that contain level data and JSON stuff
     public List<TextAsset> questionList;
@@ -62,6 +64,9 @@ public class WordGameScript : MonoBehaviour {
     }
 	
 	// Update is called once per frame
+    /**
+     *  Controls the game states and checks for the single debugging input
+     */
 	void Update () {
         if (isLose && playable)
         {
@@ -79,6 +84,9 @@ public class WordGameScript : MonoBehaviour {
 		
 	}
 
+    /**
+     * Initializes the WordGameSystem or fetches it if it exists already
+     */
     private void initSystem()
     {
         if (!GameObject.Find("wordGameSystem"))
@@ -92,6 +100,10 @@ public class WordGameScript : MonoBehaviour {
         }
     }
 
+    /**
+     * Called after level restart or after beating a level
+     * Resets every game state variable to their default value
+     */
     private void resetGameState()
     {
         if(currentLevel == 0)
@@ -106,7 +118,10 @@ public class WordGameScript : MonoBehaviour {
         playable = true;
         MusicPlayer.PlayMusic(MusicTrack.BubbleWarehouse);
     }
-
+    
+    /**
+     * Initializes UI elements, questions and game state controllers
+     */
     private void initGame()
     {
         initButtons();
@@ -118,6 +133,9 @@ public class WordGameScript : MonoBehaviour {
         updateButtonImages();
     }
 
+    /**
+     * Increments the fail count by 1 and changes the game state to LOSE afterwards.
+     */
     void addFail()
     {
         fails++;
@@ -128,6 +146,9 @@ public class WordGameScript : MonoBehaviour {
         }
     }
 
+    /**
+     * Increments the count of correct answers by 1 and plays a sound effect. If the count >= 3, this function changes the game state to WIN afterwards.
+     */
     void addCorrect()
     {
         corrects++;
@@ -142,6 +163,9 @@ public class WordGameScript : MonoBehaviour {
         }
     }
 
+    /**
+     * Game state that is triggered if the user's answer was incorrect. Prompts the user to play again or quit.
+     */
     void gameOver()
     {
         Debug.Log("Game over");
@@ -150,6 +174,11 @@ public class WordGameScript : MonoBehaviour {
         GameOverPanel.SetActive(true);
     }
 
+    /**
+     * Game state that is triggered if the user's answer was correct.
+     * Prompts the user that they have acquired a cat if not collected before.
+     * Also prompts the user to play the next level or to quit.
+     */
     void gameWin()
     {
         Debug.Log("You win!");
@@ -162,36 +191,66 @@ public class WordGameScript : MonoBehaviour {
         }
         else
         {
+            system.clearlevel(currentLevel);
             activateWinPrompt();
         }
     }
 
+    /**
+     * Activates the prompt that informs the user they have acquired a cat.
+     * Disables all the other prompts.
+     */
     public void activateCatPrompt()
     {
         GameOverPanel.SetActive(false);
-        NextLevelPanel.SetActive(true);
+        NextLevelPanel.SetActive(false);
+        FinalPanel.SetActive(false);
         CatPanel.SetActive(true);
     }
 
+    /**
+     * Activates the prompt that lets the user know they have cleared a level.
+     * If the cleared level was the last level, the game activates a special prompt.
+     * Disables all the other prompts.
+     */
     public void activateWinPrompt()
     {
-        CatPanel.SetActive(false);
-        GameOverPanel.SetActive(false);
-        NextLevelPanel.SetActive(true);
-        MusicPlayer.PlayMusic(MusicTrack.VictoryJingle);
+        if(currentLevel < 9)
+        {
+            CatPanel.SetActive(false);
+            GameOverPanel.SetActive(false);
+            FinalPanel.SetActive(false);
+            NextLevelPanel.SetActive(true);
+            MusicPlayer.PlayMusic(MusicTrack.VictoryJingle);
+        }
+        else if(currentLevel == 9)
+        {
+            CatPanel.SetActive(false);
+            GameOverPanel.SetActive(false);
+            NextLevelPanel.SetActive(false);
+            FinalPanel.SetActive(true);
+            MusicPlayer.PlayMusic(MusicTrack.VictoryJingle);
+        }
+        
     }
 
+    /**
+     * A function that buttons A, B & C call after being pressed by the user.
+     * The parameter is the button's name and it is used to determine if the answer was correct or not and the UI is updated based on the evaluation.
+     * Every click the number of tries is updated in order to keep track for the UI elements.
+     * If the click doesn't result in LOSE or WIN states (if the game goes on) we load a new question for the user.
+     */
     public void onButtonClick(string buttonName)
     {
         if (playable)
         {
             tries++;
-            if (isCorrectAnswer(buttonName))
+            if (isCorrectAnswer(buttonName))    //if the answer was correct
             {
                 addCorrect();
                 updateStarImages(true);
             }
-            else
+            else                                //if the answer was incorrect
             {
                 addFail();
                 updateStarImages(false);
@@ -202,6 +261,10 @@ public class WordGameScript : MonoBehaviour {
      
     }
 
+    /**
+     * If the gameOverPrompt is active, the buttons on the prompt call this function.
+     * The user can either restart the level or quit to the level selection screen.
+     */
     public void onGameOverClick(string buttonName)
     {
         if(buttonName == "r")
@@ -214,6 +277,10 @@ public class WordGameScript : MonoBehaviour {
         }
     }
 
+    /**
+     * A function that evaluates if the user's answer was correct.
+     * Returns a boolean value.
+     */
     private bool isCorrectAnswer(string button)
     {
         if (currentQ.answer == button)
@@ -222,6 +289,11 @@ public class WordGameScript : MonoBehaviour {
             return false;
     }
 
+    /**
+     * Parses the JSON files into an list that contains all the question data.
+     * The given parameter defines the question that needs to be loaded and added to the list.
+     * level[0] = level 1 question data, level[5] = level 6 question data, etc.
+     */
     public void loadQuestionData(string level)
     {
         questions = JsonUtility.FromJson<WordGameQuestionData>(questionList[Int32.Parse(level)-1].text);
@@ -229,6 +301,10 @@ public class WordGameScript : MonoBehaviour {
         Debug.Log("Level " + level + " questionData loaded!");
     }
     
+    /**
+     * Reloads every buttons' images based on the current question and the options available.
+     * Options are defined in every questions' JSON file.
+     */
     private void updateButtonImages()
     {
         buttonA.sprite = Resources.Load<Sprite>(currentQ.optionA);
@@ -236,6 +312,10 @@ public class WordGameScript : MonoBehaviour {
         buttonC.sprite = Resources.Load<Sprite>(currentQ.optionC);
     }
 
+    /**
+     * Updates the stars based on the number of tries and if the player's answer was correct or not.
+     * The star will turn from empty into yellow one if the answer was correct and to a crossed one if the answer was incorrect.
+     */
     private void updateStarImages(bool wasCorrect)
     {
         int cor = (wasCorrect) ? 1 : 0;
@@ -245,7 +325,7 @@ public class WordGameScript : MonoBehaviour {
          *  If cor = 0, the updated star will be crossed over
          */
 
-        if (tries == 1)
+        if (tries == 1)         //if star 1 needs to be updated
         {
             switch (cor)
             {
@@ -259,7 +339,7 @@ public class WordGameScript : MonoBehaviour {
                 
         }
 
-        if (tries == 2)
+        if (tries == 2)         //if star 2 needs to be updated
         {
             switch (cor)
             {
@@ -271,7 +351,7 @@ public class WordGameScript : MonoBehaviour {
                     break;
             }
         }
-        if (tries == 3)
+        if (tries == 3)         //if star 3 needs to be updated
         {
             switch (cor)
             {
@@ -285,12 +365,18 @@ public class WordGameScript : MonoBehaviour {
         }
     } 
 
+    /**
+     * A function that loads questions for all levels and calls another function that parses and adds the questions into a list.
+     */
     private void initQuestionList()
     {
         for (int i = 0; i < questionList.Capacity; i++)
             loadQuestionData((i+1).ToString());
     }
 
+    /**
+     * Initializes all the buttons so that by referencing them we can alter the images that are displayed on the buttons.
+     */
     private void initButtons()
     {
         buttonA = GameObject.Find("ButtonA/ButtonImageA").GetComponent<Image>();
@@ -298,6 +384,9 @@ public class WordGameScript : MonoBehaviour {
         buttonC = GameObject.Find("ButtonC/ButtonImageC").GetComponent<Image>();
     }
 
+    /**
+     * Initializes all the stars that represent the user's progress throughout the levels.
+     */
     private void initStars()
     {
         star1 = GameObject.Find("star1").GetComponent<Image>();
@@ -305,6 +394,9 @@ public class WordGameScript : MonoBehaviour {
         star3 = GameObject.Find("star3").GetComponent<Image>();
     }
 
+    /**
+     * Resets each stars' images back to empty star.
+     */
     private void resetStars()
     {
         star1.sprite = Resources.Load<Sprite>("UI/blkstar");
@@ -312,6 +404,10 @@ public class WordGameScript : MonoBehaviour {
         star3.sprite = Resources.Load<Sprite>("UI/blkstar");
     }
 
+    /**
+     * Checks the current level index and loads the question data into the currentQ variable.
+     * Updates the question text and the buttons that are used to represent the options to the user.
+     */
     private void updateCurrentQuestion()
     {
         currentQ = levels[currentLevel].questionData[getNextQIndex()];
@@ -320,6 +416,11 @@ public class WordGameScript : MonoBehaviour {
         updateButtonImages();
     }
 
+    /**
+     * Creates a list of random integers in range of 0 to 4 (5 numbers in total) and contains no dublicates.
+     * Each level contains 5 possible questions and this list is used to determine in which order the questions are presented to the user.
+     * This function also takes care that when a level is restarted, the user won't see the same question twice in a row.
+     */
     private void initQuestionIndexes()
     {
         questionIndexes.Clear();
@@ -327,30 +428,35 @@ public class WordGameScript : MonoBehaviour {
         while(questionIndexes.Count < 5)
         {
             int nextIndex = rnd.Next(5);                //nextIndex = 0 <= x < 5
-            if (!questionIndexes.Contains(nextIndex))
+            if (!questionIndexes.Contains(nextIndex))   //if the index is not already in the list
             {
-                if(questionIndexes.Count == 0 && nextIndex == lastIndex)
+                if(!(questionIndexes.Count == 0 && nextIndex == lastIndex))     //if the index is valid for the list
                 {
-                    Debug.Log("Rerolling index");
+                    questionIndexes.Add(nextIndex);
                 }
-                else questionIndexes.Add(nextIndex);
             }
         }
     }
 
+    /**
+     * Returns the first integer from the questionIndexes list.
+     */
     private int getNextQIndex()
     {
-        if (questionIndexes.Count > 0)
+        if (questionIndexes.Count > 0)          //if the list is not empty
         {
             int nextIndex = questionIndexes[0];
-            lastIndex = nextIndex;
-            questionIndexes.Remove(nextIndex);
+            lastIndex = nextIndex;              //keeps track of the current question for the next reindexing of the questionList 
+            questionIndexes.Remove(nextIndex);  //removes the returned value from the list
             return nextIndex;
         }
-        else Debug.LogError("No more questions available!");
+        else Debug.LogError("No more questions available!");        //this should not happen
         return -1;
     }
 
+    /**
+     * Resets the current level by reseting the game state, reindexing the question list and reseting the questions and buttons.
+     */
     private void resetLevel()
     {
         resetGameState();
@@ -361,6 +467,9 @@ public class WordGameScript : MonoBehaviour {
         Debug.Log("Level restarted");
     }
 
+    /**
+     * Returns the user to the levels select screen fo the quiz minigame.
+     */
     public void exitMinigame()
     {
         MusicPlayer.instance.PlaySoundEffect(MusicPlayer.instance.dontBuy, 1);
@@ -368,6 +477,9 @@ public class WordGameScript : MonoBehaviour {
         SceneManager.LoadScene("WordGameLevelSelect", LoadSceneMode.Single);
     }
 
+    /**
+     * Increments the currentLevel variable up to 9 and "resets" the level which means that the game loads new questions and resets the game state and UI to represent the new level.
+     */
     private void loadNextLevel()
     {
         if (currentLevel < levels.Capacity - 1)
@@ -379,11 +491,25 @@ public class WordGameScript : MonoBehaviour {
         else Debug.Log("No next level exists");
     }
 
+    /**
+     * Used to clear the prompt that is brought up after clearing the final level.
+     * Return the player to the level select screen.
+     */
+    public void clearOK()
+    {
+        FinalPanel.SetActive(false);
+        exitMinigame();
+    }
+
+    /**
+     * Resets the stars back to "empty" and hides any prompt that could be activated.
+     */
     private void resetUI()
     {
         resetStars();
         GameOverPanel.SetActive(false);
         NextLevelPanel.SetActive(false);
         CatPanel.SetActive(false);
+        FinalPanel.SetActive(false);
     }
 }
